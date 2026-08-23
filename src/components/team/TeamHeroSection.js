c715007f9cd2750be5557017ba23531d22a70b6b
Amaan Farooq <amkhqaz@gmail.com>
2026-08-23 16:51:42 +0530

    preparing for dokku deployment2

--- src/components/team/TeamHeroSection.js
+++ src/components/team/TeamHeroSection.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import teamHeroImage from '../../assets/images/team-hero.jpg';
+import teamHeroImage from '../../assets/images/team-hero.JPG';
 
 gsap.registerPlugin(ScrollTrigger);
 
